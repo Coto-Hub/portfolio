53e9fd9b4abfe18cb78ccbd3928ff440bf059ec5
Coto (Thomas Marty) <marty.thomas.2001@gmail.com>
2024-08-29 14:30:57 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <headerNav :navInfo="navInfo" />
-    
+    <!--     
     <section class="container">
       <router-view :player="player" :statistics="statistics" :navInfo="navInfo" :notificationCenter="notificationCenter"></router-view>
     </section>
@@ -10,7 +10,7 @@
     
     <cookie />
     <contactModal :navInfo="navInfo" :notificationCenter="notificationCenter" v-if="navInfo.showContactModal" />
-    <notificationList :notificationCenter="notificationCenter" />
+    <notificationList :notificationCenter="notificationCenter" /> -->
   </div>
 </template>
 
@@ -25,48 +25,48 @@ import { Statistics } from "./assets/js/statistics";
 import { NotificationCenter, Notification } from "./assets/js/notification";
 
 export default {
-    name: 'App',
-    components: {
-      cookie,
-      headerNav,
-      footerNav,
-      contactModal,
-      notificationList,
-    },
-    data: function () {
-      return {
-        player: {},
-        statistics: {},
-        notificationCenter: {},
-        navInfo: {
-          showContactModal: false,
-        },
-      }
-    },
-    mounted() {
-    },
-    created() {
-      this.generatePlayer();
-      this.notificationCenter = new NotificationCenter();
-    },
-    computed: {
-      getNotificationCenter() {
-        return this.notificationCenter;
-      }
-    },
-    methods: {
-      generatePlayer() {
-        this.player = new Player();
-        this.statistics = new Statistics(this.player);
+  name: 'App',
+  components: {
+    cookie,
+    headerNav,
+    footerNav,
+    contactModal,
+    notificationList,
+  },
+  data: function () {
+    return {
+      player: {},
+      statistics: {},
+      notificationCenter: {},
+      navInfo: {
+        showContactModal: false,
       },
-      addNotif() {
-        this.notificationCenter.addNotification(new Notification("icon", {
-          title: "Messagerie",
-          message: "Le mail a bien été envoyé !",
-          icon: "contact",
-          success: true,
-        }));
-      }
     }
+  },
+  mounted() {
+  },
+  created() {
+    this.generatePlayer();
+    this.notificationCenter = new NotificationCenter();
+  },
+  computed: {
+    getNotificationCenter() {
+      return this.notificationCenter;
+    }
+  },
+  methods: {
+    generatePlayer() {
+      this.player = new Player();
+      this.statistics = new Statistics(this.player);
+    },
+    addNotif() {
+      this.notificationCenter.addNotification(new Notification("icon", {
+        title: "Messagerie",
+        message: "Le mail a bien été envoyé !",
+        icon: "contact",
+        success: true,
+      }));
+    }
+  }
 }
 </script>
